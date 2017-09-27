4d92540d3e853852ab190a4819f6372dabb7d6e4
Jim Seconde <jim.seconde@campaignamp.com>
2017-09-27 23:23:21 +0100

    Added search form

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -6,6 +6,8 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use AppBundle\Entity\Book;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class DefaultController extends Controller
 {
@@ -14,13 +16,27 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        $searchForm = $this->createFormBuilder()
+            ->add('title', TextType::class)
+            ->add('author', TextType::class)
+            ->add('year', TextType::class)
+            ->add('numeric', TextType::class)
+            ->add('search', SubmitType::class)
+            ->getForm();
+
+        $searchForm->handleRequest($request);
+
+        if ($searchForm->isSubmitted()) {
+            $searchFormData = $searchForm->getData();
+        }
+
         $repository = $this->getDoctrine()->getRepository(Book::class);
         $searchResult = $repository->findAll();
 
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-            'dataSet' => $searchResult
+            'dataSet' => $searchResult,
+            'searchForm' => $searchForm->createView()
         ]);
     }
 }
