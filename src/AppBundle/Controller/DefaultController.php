d75319daf94a2cc22ed9f51f33b2a42f55574ebf
Jim Seconde <jim.seconde@campaignamp.com>
2017-10-03 00:18:31 +0100

    close with choicetype, need distinct query

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -8,6 +8,8 @@ use Symfony\Component\HttpFoundation\Request;
 use AppBundle\Entity\Book;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class DefaultController extends Controller
 {
@@ -16,20 +18,24 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        $repository = $this->getDoctrine()->getRepository(Book::class);
+
         $searchForm = $this->createFormBuilder()
             ->add('title', TextType::class)
             ->add('author', TextType::class)
-            ->add('year', TextType::class)
-            ->add('numeric', TextType::class)
+            ->add('year', ChoiceType::class, [
+                "label" => "Year",
+                "choices" => $this->getBookYears($repository)
+                ]
+            )
+            ->add('numeric', TextType::class, ['label' => 'Genre'])
             ->add('search', SubmitType::class)
             ->getForm();
 
         $searchForm->handleRequest($request);
-        $repository = $this->getDoctrine()->getRepository(Book::class);
 
         if ($searchForm->isSubmitted()) {
             $searchFormData = $searchForm->getData();
-            dump($searchFormData);
             $query = $repository->createQueryBuilder('book');
             
             if ($searchFormData['title'] != null) {
@@ -55,11 +61,25 @@ class DefaultController extends Controller
             $dataSet = $repository->findAll();
         }
 
-
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'dataSet' => $dataSet,
             'searchForm' => $searchForm->createView()
         ]);
+    }
+
+    protected function getBookYears($repository):array
+    {
+        $allRecords = $repository->createQueryBuilder('book')
+            ->groupBy('book.year')
+            ->orderBy('book.year', 'ASC');
+
+        $returnYears = [];
+
+        foreach($allRecords as $entity){
+            $returnYears[] = $entity->getYear();
+        }
+
+        return $returnYears;
     }
 }
