60ff20b0ede5e34d51d00f0467d5aed76e3dbf54
Jim Seconde <jim.seconde@campaignamp.com>
2017-10-17 23:33:42 +0100

    will need to structure my node index properly, but making a start

---
+++ web/js/index.js
@@ -0,0 +1,8 @@
+// Holy shit, time for Jimbo to start Javascripting
+var tablesorter = require('tablesorter');
+
+$(document).ready(function() 
+    { 
+        $("#results_table").tablesorter(); 
+    } 
+); 
