b139db2ed0bd892a26da23691c55f7dcdb40c77b
Jim Seconde <jim.seconde@campaignamp.com>
2017-10-18 20:52:27 +0100

    using yarn for package manager instead of npm, ditched old js index

--- web/js/index.js
+++
@@ -1,8 +0,0 @@
-// Holy shit, time for Jimbo to start Javascripting
-var tablesorter = require('tablesorter');
-
-$(document).ready(function() 
-    { 
-        $("#results_table").tablesorter(); 
-    } 
-); 
